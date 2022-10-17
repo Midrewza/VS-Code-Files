0f87894d91dd2e24f98980af2ffeb441d2248563
Michael Garza <Midrewza@gmail.com>
2022-10-17 16:55:12 -0700

    in js file, created features for onclick function every time jump is clicked, 1 count is collected canvas is clear and the red ball moves up 25 on the canvas

--- SoloLearn Projects/Game Development with JS/scripts.js
+++ SoloLearn Projects/Game Development with JS/scripts.js
@@ -11,7 +11,22 @@ window.onload = function() {
     let btn = document.getElementById("jump");
     let count = 0;
 
-    btn.click = function() {
+    btn.onclick = function() {
         count += 1;
+        // changing the y position
+        y -= 25;
+        // clearing the canvas
+        context.clearRect(0, 0, 600, 400);
+
+        // redrawing the circle
+        context.beginPath();
+        context.arc(x, y, 50, 0, 2*Math.PI);
+        context.fillStyle="red";
+        context.fill();
+        
+        // drawing the count value
+        context.font = '25px Arial';
+        context.fillStyle = 'white';
+        context.fillText("Count: " + count, 20, 30);
     }
 }
