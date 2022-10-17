f09804f8c097f52ed3e607a0c4d624b6a9f9ff20
Michael Garza <Midrewza@gmail.com>
2022-10-17 16:16:41 -0700

    added canvas id to the canvas element created a red ball in js

--- SoloLearn Projects/Game Development with JS/scripts.js
+++ SoloLearn Projects/Game Development with JS/scripts.js
@@ -1,4 +1,13 @@
 window.onload = function() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = 300;
+    var y = 350;
+
+    context.arc(x, y, 50, 0, 2*Math.PI);
+    context.fillStyle="red";
+    context.fill();
+
     let btn = document.getElementById("jump");
     let count = 0;
 
